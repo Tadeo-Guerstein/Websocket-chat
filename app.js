47116fa33eb8a9e48b333343ee3f7172a241410d
Tadeo-Guerstein <tadeleonino@hotmail.com>
2023-10-03 16:14:47 -0300

    FIX: envío incorrecto de JSON de mensajes

--- app.js
+++ app.js
@@ -17,31 +17,31 @@ wss.on("connection", ws => {
 	activeConnections.add(ws)
 	activeConnections.forEach(connection => {
 		const jsonMessages = JSON.parse(fs.readFileSync("./logs/messages.json"))
-		const jsonUsers = JSON.parse(fs.readFileSync("./users/users.json"))
+		const jsonUsers = JSON.parse(fs.readFileSync("./logs/users.json"))
 		connection.send(JSON.stringify({messages: jsonMessages, usersLogged: jsonUsers}))
 	})
 
 	ws.on("message", res => {
 		const response = JSON.parse(res)
 		if (response.petition === "DELETE") {
-			const jsonUsers = JSON.parse(fs.readFileSync("./users/users.json"))
+			const jsonUsers = JSON.parse(fs.readFileSync("./logs/users.json"))
 			const usersFiltered = jsonUsers.filter(i => {
 				return i?.username !== response.user
 			})
-			fs.writeFileSync("./users/users.json", JSON.stringify(usersFiltered))
+			fs.writeFileSync("./logs/users.json", JSON.stringify(usersFiltered))
 			activeConnections.forEach(connection => {
-				connection.send(JSON.stringify({message: 'unlogged', usersLogged: usersFiltered}))
+				connection.send(JSON.stringify({message: "unlogged", usersLogged: usersFiltered}))
 			})
 			return
 		}
 		if (response.username) {
-			const jsonUsers = JSON.parse(fs.readFileSync("./users/users.json"))
+			const jsonUsers = JSON.parse(fs.readFileSync("./logs/users.json"))
 			const isUserFound = jsonUsers.find(i => {
 				return i?.username === response.username
 			})
 			if (!isUserFound) {
 				const newUsers = [...jsonUsers, response]
-				fs.writeFileSync("./users/users.json", JSON.stringify(newUsers))
+				fs.writeFileSync("./logs/users.json", JSON.stringify(newUsers))
 				ws.send(JSON.stringify({status: 200, message: "ok"}))
 				return
 			}
@@ -57,7 +57,7 @@ wss.on("connection", ws => {
 		const newMessages = [...jsonMessages, response]
 		fs.writeFileSync("./logs/messages.json", JSON.stringify(newMessages))
 		activeConnections.forEach(connection => {
-			connection.send(JSON.stringify({messages: jsonMessages, usersLogged: []}))
+			connection.send(JSON.stringify({messages: newMessages, usersLogged: []}))
 		})
 	})
 })
